9e9d251490fd4584f75b1bff892e23a3af02bd27
DanielSanm <danielelias3221@gmail.com>
2024-06-15 19:08:17 -0300

    creating server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const port = 3000
+const mongoose = require("mongoose");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`app listening in port ${port}`)
+})
